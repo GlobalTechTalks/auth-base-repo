74704cc83bb2ba2ab7aae8dda591d0f58990bb55
Abhishek Khanna <abhishekkhanna1995@gmail.com>
2020-05-25 15:27:26 +0530

    Revert "feat(signup code):signup code only"
    This reverts commit eb410e3aaa17dfbf7fbcad7e2be13c4d6caaee88.

--- Controllers/Saltgenerator.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require("bcrypt");
-
-module.exports = function (password) {
-    bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(password, salt, (err, hash) => {
-            return hash;
-        });
-    });
-};
